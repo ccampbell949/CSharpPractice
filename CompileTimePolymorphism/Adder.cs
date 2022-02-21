ece57ce37b745abba246463d35ccfe10df8c783a
ccampbell949 <80074996+ccampbell949@users.noreply.github.com>
2022-02-21 11:35:17 +0000

    Add files via upload

---
+++ CompileTimePolymorphism/Adder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompileTimePolymorphism
+{
+    class Adder
+    {
+        //simple class for adding numbers
+        //it uses compile time polymorphism
+
+        public int Add(int n1, int n2)
+        {
+            //add 2 integers
+            return n1 + n2;
+        }
+
+        public double Add(double n1, double n2) //can have the same name if it has different input types or number of parameters
+        {
+            //add 2 doubles
+            return n1 + n2;
+        }
+
+        public int Add(int n1, int n2, int n3)
+        {
+            //add 3 integers
+            return n1 + n2 + n3;
+        }
+
+        public double Add(double n1, double n2, double n3)
+        {
+            //add 3 doubles
+            return n1 + n2 + n3;
+        }
+    }
+}
