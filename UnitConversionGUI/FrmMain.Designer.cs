4bd7e11f04eaced07794281f42a249038b567577
ccampbell949 <80074996+ccampbell949@users.noreply.github.com>
2022-02-21 11:25:16 +0000

    Add files via upload

---
+++ UnitConversionGUI/FrmMain.Designer.cs
@@ -0,0 +1,249 @@
+﻿
+namespace UnitConversionGUI
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCaption = new System.Windows.Forms.Label();
+            this.grpInput = new System.Windows.Forms.GroupBox();
+            this.grpOutput = new System.Windows.Forms.GroupBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.rbFtoM = new System.Windows.Forms.RadioButton();
+            this.rbMitoK = new System.Windows.Forms.RadioButton();
+            this.rbKToMi = new System.Windows.Forms.RadioButton();
+            this.rbMtoF = new System.Windows.Forms.RadioButton();
+            this.grpInput.SuspendLayout();
+            this.grpOutput.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblCaption
+            // 
+            this.lblCaption.AutoSize = true;
+            this.lblCaption.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCaption.Location = new System.Drawing.Point(241, 18);
+            this.lblCaption.Name = "lblCaption";
+            this.lblCaption.Size = new System.Drawing.Size(164, 25);
+            this.lblCaption.TabIndex = 0;
+            this.lblCaption.Text = "Unit Converter";
+            this.lblCaption.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // grpInput
+            // 
+            this.grpInput.BackColor = System.Drawing.Color.LightGray;
+            this.grpInput.Controls.Add(this.rbMtoF);
+            this.grpInput.Controls.Add(this.rbKToMi);
+            this.grpInput.Controls.Add(this.btnConvert);
+            this.grpInput.Controls.Add(this.rbMitoK);
+            this.grpInput.Controls.Add(this.lblInput);
+            this.grpInput.Controls.Add(this.rbFtoM);
+            this.grpInput.Controls.Add(this.txtInput);
+            this.grpInput.Location = new System.Drawing.Point(51, 64);
+            this.grpInput.Name = "grpInput";
+            this.grpInput.Size = new System.Drawing.Size(397, 168);
+            this.grpInput.TabIndex = 1;
+            this.grpInput.TabStop = false;
+            this.grpInput.Text = "Input";
+            // 
+            // grpOutput
+            // 
+            this.grpOutput.BackColor = System.Drawing.Color.LightGray;
+            this.grpOutput.Controls.Add(this.btnReset);
+            this.grpOutput.Controls.Add(this.txtOutput);
+            this.grpOutput.Controls.Add(this.lblOutput);
+            this.grpOutput.Location = new System.Drawing.Point(51, 252);
+            this.grpOutput.Name = "grpOutput";
+            this.grpOutput.Size = new System.Drawing.Size(397, 168);
+            this.grpOutput.TabIndex = 2;
+            this.grpOutput.TabStop = false;
+            this.grpOutput.Text = "Output";
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Red;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(500, 366);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(104, 58);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(307, 126);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(84, 36);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = false;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.AutoSize = true;
+            this.btnReset.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(307, 126);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(84, 36);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(77, 32);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(31, 13);
+            this.lblInput.TabIndex = 6;
+            this.lblInput.Text = "Input";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(134, 29);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(152, 20);
+            this.txtInput.TabIndex = 7;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(77, 37);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(39, 13);
+            this.lblOutput.TabIndex = 8;
+            this.lblOutput.Text = "Output";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(138, 34);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(173, 20);
+            this.txtOutput.TabIndex = 9;
+            // 
+            // rbFtoM
+            // 
+            this.rbFtoM.AutoSize = true;
+            this.rbFtoM.Location = new System.Drawing.Point(134, 90);
+            this.rbFtoM.Name = "rbFtoM";
+            this.rbFtoM.Size = new System.Drawing.Size(93, 17);
+            this.rbFtoM.TabIndex = 10;
+            this.rbFtoM.TabStop = true;
+            this.rbFtoM.Text = "Feet to Metres";
+            this.rbFtoM.UseVisualStyleBackColor = true;
+            this.rbFtoM.CheckedChanged += new System.EventHandler(this.rbFtoM_CheckedChanged);
+            // 
+            // rbMitoK
+            // 
+            this.rbMitoK.AutoSize = true;
+            this.rbMitoK.Location = new System.Drawing.Point(134, 113);
+            this.rbMitoK.Name = "rbMitoK";
+            this.rbMitoK.Size = new System.Drawing.Size(112, 17);
+            this.rbMitoK.TabIndex = 11;
+            this.rbMitoK.TabStop = true;
+            this.rbMitoK.Text = "Miles to Kilometres";
+            this.rbMitoK.UseVisualStyleBackColor = true;
+            this.rbMitoK.CheckedChanged += new System.EventHandler(this.rbMitoK_CheckedChanged);
+            // 
+            // rbKToMi
+            // 
+            this.rbKToMi.AutoSize = true;
+            this.rbKToMi.Location = new System.Drawing.Point(134, 136);
+            this.rbKToMi.Name = "rbKToMi";
+            this.rbKToMi.Size = new System.Drawing.Size(112, 17);
+            this.rbKToMi.TabIndex = 12;
+            this.rbKToMi.TabStop = true;
+            this.rbKToMi.Text = "Kilometres to Miles";
+            this.rbKToMi.UseVisualStyleBackColor = true;
+            this.rbKToMi.CheckedChanged += new System.EventHandler(this.rbKToMi_CheckedChanged);
+            // 
+            // rbMtoF
+            // 
+            this.rbMtoF.AutoSize = true;
+            this.rbMtoF.Location = new System.Drawing.Point(134, 67);
+            this.rbMtoF.Name = "rbMtoF";
+            this.rbMtoF.Size = new System.Drawing.Size(93, 17);
+            this.rbMtoF.TabIndex = 13;
+            this.rbMtoF.TabStop = true;
+            this.rbMtoF.Text = "Metres to Feet";
+            this.rbMtoF.UseVisualStyleBackColor = true;
+            this.rbMtoF.CheckedChanged += new System.EventHandler(this.rbMtoF_CheckedChanged);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(627, 448);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.grpOutput);
+            this.Controls.Add(this.grpInput);
+            this.Controls.Add(this.lblCaption);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            this.grpInput.ResumeLayout(false);
+            this.grpInput.PerformLayout();
+            this.grpOutput.ResumeLayout(false);
+            this.grpOutput.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCaption;
+        private System.Windows.Forms.GroupBox grpInput;
+        private System.Windows.Forms.RadioButton rbMtoF;
+        private System.Windows.Forms.RadioButton rbKToMi;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.RadioButton rbMitoK;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.RadioButton rbFtoM;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.GroupBox grpOutput;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ UnitConversionGUI/FrmMain.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnitConversionGUI
+{
+    public partial class FrmMain : Form
+    {
+
+        //Declare Variables to store conversion values
+        double Input = 0, Output = 0;
+
+        //Declare string to store input type
+        String inputType = "";
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Call the exit procedure
+            Exit();
+        }
+
+        private void Exit()
+        {
+            //prompt user to confirm exit
+
+            DialogResult userResponse = MessageBox.Show("Are you sure you want to exit?",//message
+                "Exit", //caption
+                MessageBoxButtons.YesNo, //buttons
+                MessageBoxIcon.Warning); //icon
+
+            //Evaluate users response
+           if (userResponse.Equals(DialogResult.Yes))
+            {
+                //exit application
+                Application.Exit(); //dont need to code "No" on a windows form
+            }
+        }
+
+        //Function to check if form is empty
+        private bool IsEmpty()
+        {
+            //function should return true if the form is empty
+            //otherwise return false
+            
+            if (!String.IsNullOrEmpty(txtInput.Text) || !String.IsNullOrEmpty(txtOutput.Text)) //if input or output is NOT empty
+            {
+                //if either text 
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            //Check if form is empty
+            if (!IsEmpty()) //runs if IsEmpty returns false (due to !)
+            {
+                //prompt user to confirm reset
+                DialogResult userResponse = MessageBox.Show("Reset will result in loss of resuilt. Are you sure you want to reset?",//message
+                    "Reset?", //caption
+                    MessageBoxButtons.YesNo, //buttons
+                    MessageBoxIcon.Warning); //icon
+
+                //Evaluate users response
+                if (userResponse.Equals(DialogResult.Yes))
+                {
+                    //reset 
+                    Reset();
+                }
+            }
+        }
+
+        //Procedure to reset form
+        private void Reset()
+        {
+            //Clear the form
+            //Clear textboxes
+            txtInput.Text = ""; //could also set these to null
+            txtOutput.Text = "";
+
+            //reset radio buttons
+            rbFtoM.Checked = false;
+            rbMtoF.Checked = false;
+            rbKToMi.Checked = false;
+            rbMitoK.Checked = false;
+
+            //reset variables
+            Input = 0;
+            Output = 0;
+            inputType = "";
+
+            //Reset input and output labels
+
+            lblInput.Text = "Input";
+            lblOutput.Text = "Output";
+        }
+
+        private double MetresToFeet(double n1)
+        {
+            //1 Meter = 3.2808399 Foot
+
+            double result = n1 * 3.2808399;
+            return result;
+        }
+        private double FeetToMetres(double n1) //convert feet to metres
+        {
+            //1ft= 0.3048000m
+
+            double result = n1 * 0.3048000; //can also just reverse the * and / when going between units
+            return result;                  // e.g. n1 / 0.30480000
+        }
+        private double MilesToKilometres(double n1) //convert miles to kilometres
+        {
+            //1mi = 1.609344km
+
+            double result = n1 * 1.609344;
+            return result;
+        } 
+        
+        private double KilometresToMiles(double n1) //convert kilometres to miles
+        {
+            //1Km is equivalent to 0.6214 miles
+
+            double result = n1 * 0.6214;
+            return result;
+        }
+
+        private void rbFtoM_CheckedChanged(object sender, EventArgs e)
+        {
+            //get checked staus of radiobutton
+            if (rbFtoM.Checked)
+            {
+                //set input type
+                inputType = "feet";
+
+                //set label
+                lblInput.Text = "feet";
+                lblOutput.Text = "metres";
+
+            }
+        }
+
+        private void rbMitoK_CheckedChanged(object sender, EventArgs e)
+        {
+            //get checked staus of radiobutton
+            if (rbMitoK.Checked)
+            {
+                //set input type
+                inputType = "miles";
+
+                //set label
+                lblInput.Text = "miles";
+                lblOutput.Text = "kilometres";
+
+            }
+
+        }
+
+        private void rbKToMi_CheckedChanged(object sender, EventArgs e)
+        {
+            //get checked staus of radiobutton
+            if (rbKToMi.Checked)
+            {
+                //set input type
+                inputType = "kilometres";
+
+                //set label
+                lblInput.Text = "kilometres";
+                lblOutput.Text = "miles";
+
+            }
+
+        }
+        
+        private void rbMtoF_CheckedChanged(object sender, EventArgs e)
+        {
+            //get checked staus of radiobutton
+            if (rbMtoF.Checked)
+            {
+                //set input type
+                inputType = "metres";
+
+                //set label
+                lblInput.Text = "metres";
+                lblOutput.Text = "feet";
+
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            //call the convert procedure
+            ConvertInput();
+
+
+        }
+
+        private void ConvertInput()
+        {
+            //convert and store input
+            Input = Convert.ToDouble(txtInput.Text.Trim());
+
+            //use inputType to determine which function to call and pass input as a paramter
+
+            if (inputType.Equals("metres"))
+            {
+                Output = MetresToFeet(Input);
+                //Display output
+                txtOutput.Text = Output.ToString("0.0");//moved this inside if statements so output doesn't display if a conversion type has not been selected
+
+            }
+            else if (inputType.Equals("feet"))
+            {
+                Output = FeetToMetres(Input);
+                //Display output
+                txtOutput.Text = Output.ToString("0.0"); //the 0.0 will round the result to 1 decimal place
+            }                                              //this is also only for converting to a string. if you are using numbers, you need to use math.round
+
+            else if (inputType.Equals("miles"))
+            {
+                Output = MilesToKilometres(Input);
+                //Display output
+                txtOutput.Text = Output.ToString("0.0");
+            }
+
+            else if (inputType.Equals("Kilometres"))
+            {
+                Output = KilometresToMiles(Input);
+                //Display output
+                txtOutput.Text = Output.ToString("0.0");
+            }
+
+            else
+            {
+                MessageBox.Show("Please select a conversion",
+                    "Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+            }
+
+            
+        }
+
+
+
+       
+
+        
+
+
+
+       
+    }
+}
